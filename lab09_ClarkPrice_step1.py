88c7e219dc3486ab1afc60ea7d93d0f11c69350a
clarkprice27 <clarkprice27@gmail.com>
2017-04-12 09:34:45 -0500

    fixed the program to close all open files

--- lab09_ClarkPrice_step1.py
+++ lab09_ClarkPrice_step1.py
@@ -27,17 +27,6 @@ def convert_file_to_list(file):
     return file_list
 
 
-def open_file():
-    """opens the file and sends to conver to list"""
-
-    try:
-        file = open('Qsand0pts.txt', 'r')
-    except IOError:
-        print('Qsand0pts.txt does not exist')
-                
-    file_list = convert_file_to_list(file)
-    return file_list
-
 
 def print_questions(file_list):
     """prints the questions formatted for the screen"""
@@ -52,6 +41,7 @@ def print_questions(file_list):
             answer_num += 1
 
         question_num += 1
+
 
 
 def create_name_dictionary(names_file):
@@ -74,16 +64,21 @@ def create_name_dictionary(names_file):
     print(assigned_questions)
 
 
+
 def main():
     """executes the program"""
-    
-    file_list = open_file()
-##    print_questions(file_list)
+
     try:
+        file = open('Qsand0pts.txt', 'r')
+        file_list = convert_file_to_list(file)
+
         names_file = open('names.txt', 'r')
+        create_name_dictionary(names_file)
+
+        file.close()
+        names_file.close()
     except IOError:
-        print('names.txt does not exist')
-    create_name_dictionary(names_file)
+        print('One of the files does not exist')
     
     
 
